db0619ad7aed89536e22e83ce0888def87aa2678
kabaneridev <krplominski@gmmail.com>
2019-03-09 08:14:35 +0100

    Fixed bug #3

--- company/filters.py
+++ company/filters.py
@@ -7,3 +7,12 @@ class CompanyFilter(django_filters.FilterSet):
 	class Meta:
 		model = Company
 		fields = ['type', 'city', 'students']
+
+	def __init__(self, *args, **kwargs):
+		super(CompanyFilter, self).__init__(*args, **kwargs)
+		self.filters['type'].extra.update(
+			{'empty_label': 'All'})
+		self.filters['city'].extra.update(
+			{'empty_label': 'All'})
+		self.filters['students'].extra.update(
+			{'empty_label': 'All'})
